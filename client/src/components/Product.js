954bdc013e5cf4edcba5fd1d4d86232cbdcdc810
D Marcus <mindlesscorporatedrone@gmail.com>
2021-08-27 21:00:47 -0700

    got filter working for AllProducts component

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Link, useHistory } from "react-router-dom";
 
 import './_Product.scss'
 
-function Product() {
+function Product(props) {
+    const { name, image, category, price, rating } = props;
     return (
         <div className="Product">
-            <img src='https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081'></img>
-            <div className="Product-info">
-                <div className="Product-info-name">Name</div>
-                <div className="Product-info-data">
-                    <div>$1.00</div>
-                    <div>Rating</div>
+            <Link>
+                <img src={image}></img>
+                <div className="Product-info">
+                    <div className="Product-info-name">{name}</div>
+                    <div className="Product-info-data">
+                        <div>${price}</div>
+                        <div>{rating}</div>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     )
 }
--- client/src/pages/AllProducts.js
+++ client/src/pages/AllProducts.js
@@ -1,11 +1,213 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Product from '../components/Product'
 import './_AllProducts.scss'
 
 function AllProducts() {
     const [displayProducts, setDisplayProducts] = useState('All Products');
+    const [loadedProducts, setLoadedProducts] = useState([]);
     const handleClick = (type) => setDisplayProducts(type);
+
+    const dummyProducts = [
+        {
+            name: 'Glove1',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Boxing Gloves',
+            price: 85.00,
+            rating: 4,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },
+        {
+            name: 'Glove2',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'MMA Gloves',
+            price: 50.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },
+        {
+            name: 'Glove3',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Boxing Gloves',
+            price: 99.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },        {
+            name: 'Glove4',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Bag Gloves',
+            price: 99.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },
+        {
+            name: 'Glove5',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Boxing Gloves',
+            price: 99.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },
+        {
+            name: 'Glove6',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Wraps',
+            price: 30.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },
+        {
+            name: 'Glove7',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Boxing Gloves',
+            price: 85.00,
+            rating: 4,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },
+        {
+            name: 'Glove8',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'MMA Gloves',
+            price: 50.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },
+        {
+            name: 'Glove9',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Boxing Gloves',
+            price: 99.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },        {
+            name: 'Glove10',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Bag Gloves',
+            price: 99.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },
+        {
+            name: 'Glove11',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Boxing Gloves',
+            price: 99.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        },
+        {
+            name: 'Glove12',
+            description: 'Glove for boxing. ',
+            image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081',
+            category: 'Wraps',
+            price: 30.00,
+            rating: 3,
+            reviews: [
+                {
+                    username: 'React',
+                    body: 'https://reactjs.org/',
+                    rating: 3
+                }
+            ]
+        }
+    ]
+
+    useEffect(() => {
+        let displayedProducts = [];
+        dummyProducts.forEach((product) => {
+            if (product.category === displayProducts) {
+                displayedProducts.push(product)
+            } else if (displayProducts === 'All Products'){
+                displayedProducts.push(product)
+            }
+        })
+        setLoadedProducts(displayedProducts)
+    },[displayProducts])
+
+    let fetchedProducts;
+    if (loadedProducts) {
+        fetchedProducts = loadedProducts.map(product => (
+            <Product name={product.name} image={product.image} category={product.category} price={product.price} rating={product.rating} />
+        ))
+    }
+
 
     return (
         <div className="AllProducts">
@@ -15,7 +217,7 @@ function AllProducts() {
                     {displayProducts}
                 </div>
                 <ul>
-                <li onClick={() => handleClick('All Products')}>All Products</li>
+                    <li onClick={() => handleClick('All Products')}>All Products</li>
                     <li onClick={() => handleClick('Boxing Gloves')}>Boxing Gloves</li>
                     <li onClick={() => handleClick('MMA Gloves')}>MMA Gloves</li>
                     <li onClick={() => handleClick('Bag Gloves')}>Bag Gloves</li>
@@ -23,26 +225,7 @@ function AllProducts() {
                 </ul>
             </div>
             <div className="AllProducts-Products">
-                <div className="AllProducts-Products-row">
-                    <Product />
-                    <Product />
-                    <Product />
-                </div>
-                <div className="AllProducts-Products-row">
-                    <Product />
-                    <Product />
-                    <Product />
-                </div>
-                <div className="AllProducts-Products-row">
-                    <Product />
-                    <Product />
-                    <Product />
-                </div>
-                <div className="AllProducts-Products-row">
-                    <Product />
-                    <Product />
-                    <Product />
-                </div>
+                <div className="AllProducts-Products-row">{fetchedProducts}</div>
             </div>
 
         </div>
