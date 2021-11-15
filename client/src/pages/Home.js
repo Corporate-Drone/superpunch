51c42e2b0f7bfb85cdc1b78db6283d06d326efb2
D Marcus <mindlesscorporatedrone@gmail.com>
2021-11-14 18:47:10 -0800

    added input font styles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,7 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import { useHistory } from "react-router-dom";
 
-// import background from '../assets/background.jpg'
 import Button from '../components/uiElements/Button';
-import Gloves from '../assets/boxing-gloves.png'
 import Ali from '../assets/ali-bright.png'
 import './_Home.scss'
 
@@ -17,10 +15,8 @@ function Home() {
                 <h1>Gloves for heavy hitters
                     <br /> by SuperPunch
                 </h1>
-                {/* <button onClick={() => history.push('/shop')}>Shop Now</button> */}
                 <Button onClick={() => history.push('/shop')} text={'Shop Now'} />
             </div>
-            {/* <div className="Home-glove"><img src={Gloves}></img></div> */}
             <div className="Home-Ali"><img src={Ali}></img></div>
 
         </div>
